1e691b11e2535f89e72045cae4b7d87899cf64fb
Edwin Miguel <edwintriana@gmail.com>
2013-11-15 22:12:11 -0500

    first user registered

---
+++ src/main/java/db/migration/V20131115_2205__admin_user.sql
@@ -0,0 +1,8 @@
+INSERT INTO uma_user(usr_username, usr_password)
+    VALUES ('admin', 'admin');
+            
+INSERT INTO uma_email(eml_email, eml_user_id)
+    VALUES ('admin@uma.mysu.com', 1);
+
+INSERT INTO uma_default_email(dfeml_user_id, dfeml_email_id)
+    VALUES (1, 1);
