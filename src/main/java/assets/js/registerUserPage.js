420e60fab666a7bda34e6d5f87cedc7f9fe4757b
Edwin Miguel <edwintriana@gmail.com>
2013-12-09 17:06:39 -0500

    adding validation

--- src/main/java/assets/js/registerUserPage.js
+++ src/main/java/assets/js/registerUserPage.js
@@ -7,10 +7,12 @@ require.config({
     backbone:{
       deps:['jquery'],
       exports: 'Backbone'
-    }
+    },
+    'jquery.validate': ['jquery']
   },
   paths:{
-    jquery: '/assets/webjars/jquery/1.9.0/jquery',
+    jquery: '/assets/webjars/jquery/2.0.3/jquery',
+    'jquery.validate': '/assets/webjars/jquery-validation/1.11.1/jquery.validate',
     backbone: '/assets/webjars/backbonejs/1.0.0/backbone',
     underscore: '/assets/webjars/underscorejs/1.4.3/underscore',
     json: '/assets/webjars/requirejs-plugins/3ff54566f8/json',
@@ -18,7 +20,7 @@ require.config({
   }  
 });
 
-define(['jquery', 'backbone', 'underscore', 'views/registerUserView'], function($, Backbone, _, RegisterUserView){
-  alert('I came here');
+define(['jquery', 'backbone', 'underscore', 'views/registerUserView'], 
+    function($, Backbone, _, RegisterUserView){
   var view = new RegisterUserView();
 });
--- src/main/java/assets/js/views/registerUserView.js
+++ src/main/java/assets/js/views/registerUserView.js
@@ -1,10 +1,20 @@
 /**
  * 
  */
-define(['jquery', 'backbone', 'json!locale/registerUser.json'], 
-    function($, Backbone, messages){
+define(['jquery', 'jquery.validate', 'backbone', 'json!locale/registerUser.json'], 
+    function($, Validator, Backbone, messages){
 
   var RegisterUserView = Backbone.View.extend({
+    initialize: function(){
+      $('#registerUserForm').validate();
+    },
+    events: {
+      'keypress #username': 'validateUsername'
+    },
+    
+    validateUsername: function(){
+      
+    }
     
   });
   
